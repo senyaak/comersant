e138c00ccd59d5a7fcae9344993a933a097605bd
Senyaak <senya-arseniy@mail.ru>
2023-10-25 21:45:36 +0200

    Add frontend files and docs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  /**
+   * Retrieves the hello message.
+   *
+   * @returns {string} The hello message.
+   */
   @Get()
   getHello(): string {
     return this.appService.getHello();
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'comersant-frontend', 'documentation'),
+      serveRoot: '/docs/frontend',
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'documentation'),
+      serveRoot: '/docs/backend',
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'src', 'static'),
+      serveRoot: '/docs/',
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(
+        __dirname,
+        '..',
+        'comersant-frontend',
+        'dist',
+        'comersant-frontend',
+      ),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
